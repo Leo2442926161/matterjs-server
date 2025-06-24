e08dec388c17ccd88a9b697b329084dbb319aa2a
Ingo Fischer <github@fischer-ka.de>
2025-06-24 09:03:23 +0200

    update to matter.js 0.14

--- src/model/ModelMapper.ts
+++ src/model/ModelMapper.ts
@@ -40,6 +40,9 @@ export const ClusterMap: {
 
 // Remap the clusters from Model  to a more optimized form for direct access
 MatterModel.standard.clusters.forEach(cluster => {
+    if (cluster.id === undefined) {
+        return;
+    } // Skip clusters without an ID
     const aces = cluster.allAces;
     const clusterData: ClusterMapEntry = {
         clusterId: ClusterId(cluster.id),
